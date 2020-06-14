5adc46a192cb6b4352f0068f8e2e1f4dc589f22c
Oracles3 <64440554+Oracles3@users.noreply.github.com>
2020-06-14 16:56:41 +1200

    Add files via upload

---
+++ Footer.php
@@ -0,0 +1,8 @@
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+
+<footer>
+    <p>Assessment 2</p>
+    <p>Unitec</p>
+    <p>Created by Stephan Teaca-Jucan</p>
+</footer>
