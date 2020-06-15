27695433bb87c3dd57bc5a002d0f5655c2460449
Oracles3 <64440554+Oracles3@users.noreply.github.com>
2020-06-15 14:04:27 +1200

    Add files via upload

--- Product.php
+++ Product.php
@@ -22,19 +22,19 @@
             </div>
 
             <div class="nav-link">
-                <a href="Aboutus.php">Dogs</a>
+                <a href="Aboutus.php">About Us</a>
             </div>
 
             <div class="nav-link">
-                <a href="Category.php">Birds</a>
+                <a href="Category.php">Category</a>
             </div>
 
             <div class="nav-link active-nav-link">
-                <a href="Product.php">Rabbits</a>
+                <a href="Product.php">Product</a>
             </div>
 
             <div class="nav-link ">
-                <a href="Invoice.php">Cats</a>
+                <a href="Invoice.php">Invoice</a>
             </div>
 
             <!-- Brand Logo with the shops name -->
