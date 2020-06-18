c899f9bd22fd5e71a5df54cebcab3e13c4ac6c1d
Oracles3 <64440554+Oracles3@users.noreply.github.com>
2020-06-18 20:22:47 +1200

    Add files via upload

--- Product.php
+++ Product.php
@@ -7,6 +7,46 @@
     $database = "md5o42h5gjgc4hwo";
 
     $con = new mysqli($host,$user,$password,$database );
+if (isset($_POST["add"])){
+    if (isset($_SESSION["cart"])){
+        $item_array_id = array_column($_SESSION["cart"],"product_id");
+        if (!in_array($_GET["id"],$item_array_id)){
+            $count = count($_SESSION["cart"]);
+            $item_array = array(
+                'product_id' => $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'product_price' => $_POST["hidden_price"],
+                'item_quantity' => $_POST["quantity"],
+            );
+            $_SESSION["cart"][$count] = $item_array;
+            echo '<script>window.location="Product.php"</script>';
+        }else{
+            echo '<script>alert("Product is already Added to Cart")</script>';
+            echo '<script>window.location="Product.php"</script>';
+        }
+    }else{
+        $item_array = array(
+            'product_id' => $_GET["id"],
+            'item_name' => $_POST["hidden_name"],
+            'product_price' => $_POST["hidden_price"],
+            'item_quantity' => $_POST["quantity"],
+        );
+        $_SESSION["cart"][0] = $item_array;
+    }
+}
+
+if (isset($_GET["action"])){
+    if ($_GET["action"] == "delete"){
+        foreach ($_SESSION["cart"] as $keys => $value){
+            if ($value["product_id"] == $_GET["id"]){
+                unset($_SESSION["cart"][$keys]);
+                echo '<script>alert("Product has been Removed...!")</script>';
+                echo '<script>window.location="Product.php"</script>';
+            }
+        }
+    }
+}
+?>
 ?>
 
 
@@ -21,7 +61,7 @@
     <script src="main.js" async></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    
+
 </head>
 <body>
 <div class="announcement-bar">
@@ -29,7 +69,7 @@
 </div>
 
 <!-- Nav link -->
-<div class="container">
+<div class="contain">
     <div class="nav-wrapper">
         <div class="left">
             <div class="nav-link">
